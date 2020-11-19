e3daa69ef844ba2e5eec9e2b35c4b21aa5ef1aec
Advaith Mopuri <advaith@advaiths-imac.lan>
2020-11-19 12:10:03 -0800

    commit WhereAmI.cpp to the Main Branch

---
+++ whereAmI.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <set>
+using namespace std;
+ 
+int N;
+string S;
+bool dups(int len)
+{
+  set<string> X;
+  for (int i=0; i<=N-len; i++) {
+    if (X.count(S.substr(i,len)) > 0) return true;
+    X.insert(S.substr(i,len));
+  }
+  return false;
+}
+int main(void)
+{
+  cin >> N >> S;
+  int ans = 1;
+  while (dups(ans)) ans++;
+  cout << ans << "\n";
+  return 0;
+}
