c0e6fcd700ef3374ab9324928b1b7bc5f50f5ed7
Advaith Mopuri <advaith@advaiths-imac.lan>
2020-11-19 16:53:11 -0800

    commit to Main

--- milkFactory.cpp
+++ milkFactory.cpp
@@ -15,8 +15,8 @@ int main()
  
   int answer = -1;
   for (int i=1; i<=N; i++) {
-    if (outgoing[i]==0 && answer != -1 ) { answer = -1; break; } // found two sinks -- bad!
-    if (outgoing[i]==0) answer = i;  // found first sink; remember it
+    if (outgoing[i]==0 && answer != -1 ) { answer = -1; break; } 
+    if (outgoing[i]==0) answer = i;  
   }
   cout << answer << "\n";
   return 0;
