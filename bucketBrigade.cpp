d58f8da0c82172a07a026e8b64cfa2afdd25a56c
Advaith Mopuri <advaith@advaiths-imac.lan>
2021-01-13 07:43:54 -0800

    commit on main

--- bucketBrigade.cpp
+++ bucketBrigade.cpp
@@ -1,33 +1,45 @@
 #include <iostream>
-#include <cmath>
 #include <fstream>
+#include <cmath>
 using namespace std;
- 
-int barn_i, barn_j, rock_i, rock_j, lake_i, lake_j;
- 
+
 int main()
 {
+  //I is row
+  //J is column
   ifstream fin("buckets.in");
   ofstream fout("buckets.out");
-  for (int i=0; i<10; i++) {
-    string s;
-    fin >> s;
-    for (int j=0; j<10; j++) {
-      if (s[j] == 'B') { barn_i = i; barn_j = j; }
-      if (s[j] == 'R') { rock_i = i; rock_j = j; }
-      if (s[j] == 'L') { lake_i = i; lake_j = j; }
+  char layout[10][10];
+  int barnI, barnJ, lakeI, lakeJ, rockI, rockJ;
+  for(int i=0; i<10; i++)
+  {
+    for(int j=0; j<10; j++)
+    {
+      fin >> layout[i][j];
+      if(layout[i][j]=='B')
+      {
+        barnI = i;
+        barnJ = j;
+      }
+      if(layout[i][j]=='R')
+      {
+        rockI = i;
+        rockJ = j;
+      }
+      if(layout[i][j]=='L')
+      {
+        lakeI = i;
+        lakeJ = j;
+      }
     }
   }
- 
-  int dist_br = abs(barn_i - rock_i) + abs(barn_j - rock_j);
-  int dist_bl = abs(barn_i - lake_i) + abs(barn_j - lake_j);
-  int dist_rl = abs(rock_i - lake_i) + abs(rock_j - lake_j);
-
-  // Check for special case where rock is between barn and lake  
-  if ((barn_i==lake_i || barn_j==lake_j) && dist_bl == dist_br + dist_rl)
-    fout << dist_bl + 1 << "\n";
-  else
-    fout << dist_bl - 1 << "\n";
- 
-  return 0;
+  if(barnI==rockI==lakeI)
+  {
+    fout << abs(lakeI-barnI) + abs(lakeJ-barnJ) + 1;
+  } else if(barnJ==rockJ==lakeJ)
+  {
+    fout << abs(lakeI-barnI) + abs(lakeJ-barnJ) + 1;
+  } else {
+    fout << abs(lakeI-barnI) + abs(lakeJ-barnJ) - 1;
+  }
 }
